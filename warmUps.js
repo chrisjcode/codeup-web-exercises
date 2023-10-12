08e96530a94847d29b62ddcad088a25efbf36b40
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-10-12 13:54:54 -0500

    add date to warm ups

--- warmUps.js
+++ warmUps.js
@@ -1,6 +1,8 @@
 // Christopher Jackson
 //  Daily Warmups
 
+// Oct 12
+
 let firstName = "Christopher";
 
 let lastName = "Jackson";
@@ -10,3 +12,4 @@ let graduationYear = '2005';
 let message = `My name is ${firstName + lastName}, and I graduted highscool ${graduationYear}`;
 
 console.log(message);
+
