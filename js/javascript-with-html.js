cb2dcd3eba88b52896a1df0e0cb9a3c9c9903f37
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-10-12 16:09:40 -0500

    javascript-with-html complete

---
+++ js/javascript-with-html.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let myAge = 5;
+
+console.log('myAge');
+
+alert("Hello Lasso");
+
+// confirm
+let confirmed = confirm("Did you really want to do that");
+
+// prompt
+let userInput = prompt("Type in your name");
+
+console.log(`Great to meet you, ${userInput}`)
+
+console.log("external js file");
+
+let myName = "Cj"
+let myAge = 36;
+console.log(`Im ${myName} and Im ${myAge} years old.`)
