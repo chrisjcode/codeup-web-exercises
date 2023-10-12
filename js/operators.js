2d2b199f98370166aeef08b5693e087e27b4412a
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-10-12 16:45:26 -0500

    operators complete

--- js/operators.js
+++ js/operators.js
@@ -1,2 +1,41 @@
 // Operators
-
+// expected output
+'1' + 2
+// 12
+"" ** 4
+// NaN - Nope its actually 0
+typeof '1' + 2
+// 'string
+6 % 4
+// 2
+'3 + 4 is ' + 3 + 4
+//'3 + 4 is 34'
+// how could you make this produce the correct output?
+0 < 0
+//false
+'false' == false
+//false
+true == 'true'
+//false
+5 >= -5
+//true
+!false || false
+//true
+true || "42"
+//true
+!true && !false
+//
+6 % 5
+// 1
+5 < 4 && 1 === 1
+// false
+typeof 'codeup' === 'string'
+// true
+'codeup' === 'codeup' && 'codeup' === 'Codeup'
+//false
+4 >= 0 && 1 !== '1'
+//true
+6 % 3 === 0
+// true
+5 % 2 !== 0
+// true
