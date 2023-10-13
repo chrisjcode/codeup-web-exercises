22557cccd01f843cd5784e565b2fde3abec4c017
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-10-13 15:18:44 -0500

    functions exercises complete

--- js/functions.js
+++ js/functions.js
@@ -9,6 +9,8 @@
  * > sayHello("codeup") // returns "Hello, codeup!"
  */
 
+ let sayHello = (name) =>  `Hello ${name}!`;
+
 /**
  * TODO:
  * Call the function 'sayHello' and pass your name as a string literal argument.
@@ -16,14 +18,16 @@
  *
  * console.log 'helloMessage' to check your work
  */
-
+sayHello('Christopher Jackson');
 /**
  * TODO:
  * Store your name as a string in a variable named 'myName', and pass that
  * variable to the 'sayHello' function. You should see the same output in the
  * console.
  */
+let myName = "Christopher Jackson";
 
+sayHello(myName);
 // Don't modify the following line, it generates a random number between 1 and 3
 // and stores it in a variable named random
 const random = Math.floor((Math.random() * 3) + 1);
@@ -45,6 +49,8 @@ const random = Math.floor((Math.random() * 3) + 1);
  * different result everytime you refresh the page if you are using the random
  * number)
  */
+let isTwo = (num) =>  num === 2;
+console.log(isTwo(random));
 
 /**
  * TODO:
@@ -57,13 +63,20 @@ const random = Math.floor((Math.random() * 3) + 1);
  * > calculateTip(0.25, 25.50) // returns 6.375
  * > calculateTip(0.15, 33.42) // returns 5.013
  */
-
+let calculateTip = (tipPercentage, bill) => bill*tipPercentage;
+calculateTip(0.20, 20) // returns 4
 /**
  * TODO:
  * Use prompt and alert in combination with your calculateTip function to
  * prompt the user for the bill total and a percentage they would like to tip,
  * then display the dollar amount they should tip
  */
+
+let customerBill = prompt('How much was your total bill?');
+let customerTipPercentageRequested = prompt('What percentage tip would you like to leave?');
+let tipTotal =  customerTipPercentageRequested ? (customerTipPercentageRequested/100) * customerBill : customerBill
+
+alert(`Your tip total should be $ ${tipTotal.toFixed(2)}`);
 
 /**
  * TODO:
@@ -79,3 +92,7 @@ const random = Math.floor((Math.random() * 3) + 1);
  *
  * > applyDiscount(45.99, 0.12) // 40.4712
  */
+
+let applyDiscount = (initialPrice, discountPercentage) => (initialPrice - (initialPrice * discountPercentage)).toFixed(2);
+
+applyDiscount(100, .2);
