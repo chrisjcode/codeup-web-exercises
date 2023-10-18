0831dc17e4abbcb1c2c0d8b8670e37953b9b1b87
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-10-18 16:41:26 -0500

    while loop complete edit

--- js/while.js
+++ js/while.js
@@ -1,8 +1,8 @@
 let num = 2;
 
-while (num <= 65536){
-    console.log(num)
+while (num < 65536){
     num *= 2;
+    console.log(num);
 }
 
 //Ice cream Cones
@@ -12,24 +12,24 @@ let customerSales = 0
 let failedCustomerSails = 0;
 
 let startingInventory = Math.floor(Math.random() * 50) + 50;
-let coneInventory = startingInventory;
+let allCones = startingInventory;
 do{
    let customerConeOrder = Math.floor(Math.random() * 5) + 1;
     totalCustomerSalesAttempts++;
 
-    if(coneInventory >= customerConeOrder){
+    if(allCones >= customerConeOrder){
         customerSales++;
-        console.log(`I have ${coneInventory} cones left, so I can sell you ${customerConeOrder}. Enjoy.`);
-        coneInventory -= customerConeOrder;
+        console.log(`I have ${allCones} cones left, so I can sell you ${customerConeOrder}. Enjoy.`);
+        allCones -= customerConeOrder;
     }else{
         failedCustomerSails++;
         console.log(`I'm sorry I don't have ${customerConeOrder} cones left to sell you.`)
     }
 
-}while(coneInventory)
+}while(allCones)
 
 failedCustomerSails = totalCustomerSalesAttempts - customerSales;
-console.log(`Starting Cone Inventory: ${startingInventory}`);
+console.log(`Starting Cone Inventory: ${startingInventory} cones have been sold.`);
 console.log(`Total Customers Sold: ${customerSales}`);
 console.log(`Total Customers Not Getting Ice Cream: ${failedCustomerSails}`);
 console.log(`All Sale Attempts: ${totalCustomerSalesAttempts}`);
