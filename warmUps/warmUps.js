3fee6b302f61104b6cd34e0f911042bd56267b2a
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-10-19 09:30:47 -0500

    warmups oct19

--- warmUps/warmUps.js
+++ warmUps/warmUps.js
@@ -13,3 +13,12 @@ let message = `My name is ${firstName + lastName}, and I graduted highscool ${gr
 
 console.log(message);
 
+// warmups oct 19
+
+let num = 100;
+
+while (num) {
+    num -= 5;
+    console.log(!(num % 2) ? `${num} is even` : num);
+}
+
