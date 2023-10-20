9478508bacd594b23228c05b302d5375dcdacded
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-10-20 16:00:26 -0500

    complete planet string

--- js/arrays/planets-string.js
+++ js/arrays/planets-string.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     "use strict";
 
     let planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
@@ -10,7 +10,7 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
-let
+    planetsArray = planetsString.split('|');
     console.log(planetsArray);
 
     /**
@@ -18,10 +18,31 @@ let
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
      *
+     *A way to format on  web pages
+     *
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    let webPlanetString = "";
+
+    planetsArray.forEach((planet) => {
+        webPlanetString += planet.concat(`<br>`);
+    });
+
+    console.log(webPlanetString);
+
+    let undorderedPlanetList = `<ul>`;
+
+    planetsArray.forEach((planet, index) => {
+        let loopPlanetHtml = `<li>${planet}</li>`
+        undorderedPlanetList += loopPlanetHtml;
+        if (index === planetsArray.length - 1) {
+            undorderedPlanetList = undorderedPlanetList.concat(`</ul>`);
+        }
+    });
+    console.log(undorderedPlanetList);
 
 })();
