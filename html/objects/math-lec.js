198793a566468f8cf1c42e404bea2ae2ad672d8b
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-10-23 15:27:30 -0500

    math lec

---
+++ html/objects/math-lec.js
@@ -0,0 +1,18 @@
+// rounds to nearest integer
+Math.round(23.4)
+
+// rounds to the lesser value
+Math.floor()
+
+// rounds to nearest integer if decimal is greater than zero
+
+Math.ceil()
+
+//exponents
+Math.pow(2,16)
+
+// gets pi value
+Math.PI()
+
+//  log base
+Math.E
