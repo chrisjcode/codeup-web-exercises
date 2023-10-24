da6c877f3414e49dd480e772ef678ccc06619792
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-10-24 10:15:33 -0500

    date exercise

---
+++ js/objects/date-lec.js
@@ -0,0 +1,15 @@
+// Create Date
+
+let today = new Date();
+
+let codeUpDate = new Date();
+
+// DateConstructor.(year: number,
+// monthIndex: number,
+// date ? : number,
+// hours ? : number,
+// minutes ? : number, seconds ? : number,
+// ms ? : number
+// )
+
+codeUpDate = new Date(l, l);
