58e5231536c1c99e2a4f643232b25eddd99ccc9f
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-11-21 16:18:54 -0600

    traverse complete

--- DOM/exercises/js/parks.js
+++ DOM/exercises/js/parks.js
@@ -1,7 +1,25 @@
 // Under the FAQ, add 3 unordered lists like above. Each list should contain a national park name in an h3 element, and a ul of 4 facts about the park.
 //
-//     Create a button that, when clicked, makes the last li in each ul have a yellow background.
-//
-//     When any h3 is clicked, the lis underneath it should have a fontWeight of "bold". Hint: you should use this in the event listener to access the next sibling of the h3 that is clicked. You can get to the corresponding ul by traversing to the h3's sibling and then it's children.
-//
-//     When any list item is clicked, the first li of that list item's parent ul should have a font color of blue. Hint: you should again rely on this in the li event handler.
+let newUl = document.getElementsByTagName('h1')[0].appendChild(document.createElement('ul'));
+
+for (let i = 0; i <= 3; i++) {
+    newUl = document.getElementsByTagName('h1')[0].appendChild(document.createElement('ul'))
+    for (let j = 0; j <= 4; j++) {
+        let li = newUl.appendChild(document.createElement('li'));
+
+        let newSpan = li.appendChild(document.createElement('span'));
+
+        newSpan.innerHTML = "Facts about Park"
+    }
+}
+
+for (let elementsByTagNameElement of document.getElementsByTagName('h3')) {
+    elementsByTagNameElement.addEventListener("click", (event) => {
+        for (const nextElementsChild of    event.target.nextElementSibling.children) {
+            // console.log(nextElementsChild);
+            nextElementsChild.style.fontWeight = 'bold';
+        }
+    })
+}
+
+
