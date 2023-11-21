d4add466bc30e27aa0b61fd42a054afa74f6eb47
Christopher Jackson <christopher.kyle.jackson@gmail.com>
2023-11-21 15:25:57 -0600

    konami complete

---
+++ DOM/exercises/js/parks.js
@@ -0,0 +1,7 @@
+// Under the FAQ, add 3 unordered lists like above. Each list should contain a national park name in an h3 element, and a ul of 4 facts about the park.
+//
+//     Create a button that, when clicked, makes the last li in each ul have a yellow background.
+//
+//     When any h3 is clicked, the lis underneath it should have a fontWeight of "bold". Hint: you should use this in the event listener to access the next sibling of the h3 that is clicked. You can get to the corresponding ul by traversing to the h3's sibling and then it's children.
+//
+//     When any list item is clicked, the first li of that list item's parent ul should have a font color of blue. Hint: you should again rely on this in the li event handler.
